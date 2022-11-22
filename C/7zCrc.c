7659dba89142f3bd2475ca9f746ebecb7a2a0eff
sisong <sisong@gmail.com>
2022-11-22 16:53:43 +0800

    fix android ndk build

--- C/7zCrc.c
+++ C/7zCrc.c
@@ -83,11 +83,11 @@ UInt32 MY_FAST_CALL CrcUpdateT1(UInt32 v, const void *data, size_t size, const U
     #endif
   #elif (defined(__clang__) && (__clang_major__ >= 3)) \
      || (defined(__GNUC__) && (__GNUC__ > 4))
-      #if !defined(__ARM_FEATURE_CRC32)
+      #if (!defined(__ARM_FEATURE_CRC32))&&(!defined(__ANDROID__))
         #define __ARM_FEATURE_CRC32 1
-          #if (!defined(__clang__) || (__clang_major__ > 3)) // fix these numbers
+        #if (!defined(__clang__) || (__clang_major__ > 3)) // fix these numbers
             #define ATTRIB_CRC __attribute__((__target__("arch=armv8-a+crc")))
-          #endif
+        #endif
       #endif
       #if defined(__ARM_FEATURE_CRC32)
         #define USE_ARM64_CRC
